dad6cb1ecd952f0d25c3f102edadae87c168d3e2
devseag <noexample@gmail.com>
2023-08-24 20:18:15 -0300

    add auth login

---
+++ db/scripts/002_ddl_create_users_table.sql
@@ -0,0 +1,9 @@
+-- drop table users;
+
+CREATE TABLE if NOT EXISTS users
+(
+    id          serial      PRIMARY KEY,
+    name        text        NOT NULL,
+    login       text        NOT NULL UNIQUE,
+    password    text        NOT NULL
+);
---
+++ src/main/java/ru/job4j/todo/filter/SessionFilter.java
@@ -0,0 +1,38 @@
+package ru.job4j.todo.filter;
+
+import java.io.IOException;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpFilter;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import ru.job4j.todo.model.User;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+@Order(2)
+public class SessionFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response,
+                            FilterChain chain) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        addUserToSession(session, request);
+        chain.doFilter(request, response);
+    }
+
+    private void addUserToSession(HttpSession session, HttpServletRequest request) {
+        User user = (User) session.getAttribute("user");
+        request.setAttribute("user", user);
+    }
+
+}
